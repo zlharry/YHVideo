f8f7d557c013222898cb933e34d7109935fcf56f
harry <yuanhua_zhong@icloud.com>
2017-06-12 16:23:02 +0800

    调整项目目录结构。

---
+++ YHVideo/Class/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHVideo
+//
+//  Created by harry on 2017/6/12.
+//  Copyright © 2017年 harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YHVideo/Class/Other/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHVideo
+//
+//  Created by harry on 2017/6/12.
+//  Copyright © 2017年 harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
